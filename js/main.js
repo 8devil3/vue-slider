d35abba03b5f8e79ebfd8b20c4b7c73b9ff40e17
8devil3 <8dev.il3@gmail.com>
2022-03-11 16:26:59 +0100

    aggiunto recupero dei dati dinamico

--- js/main.js
+++ js/main.js
@@ -2,34 +2,8 @@ const app = new Vue({
     el: '#container',
     data: {
         activeIndex: 0,
+        arrSlides: [],
         clock: '',
-        arrSlides: [
-            {
-                img: 'img/01.jpg',
-                title: 'Svezia',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-            },
-            {
-                img: 'img/02.jpg',
-                title: 'Svizzera',
-                text: 'Lorem ipsum',
-            },
-            {
-                img: 'img/03.jpg',
-                title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-                img: 'img/04.jpg',
-                title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-            },
-            {
-                img: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-            },
-        ],
     },
     methods: {
         prevImg() {
@@ -52,8 +26,14 @@ const app = new Vue({
         stopSlides() {
             clearInterval(this.clock);
         },
+        getData() {
+            for (let i = 0; i < items.length; i++) {
+                this.arrSlides.push({img:items[i], title:title[i], text:text[i]});
+            }
+        }
     },
     created(){
+        this.getData();
         this.autoSlides();
     }
 });
